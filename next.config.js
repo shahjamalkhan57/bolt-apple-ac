4b81790083c2904ae1f878a93092772c1fd87620
shahjamalkhan57 <106434450+shahjamalkhan57@users.noreply.github.com>
2025-06-05 23:57:22 +0500

    Updated next.config.js

--- next.config.js
+++ next.config.js
@@ -11,6 +11,12 @@ const nextConfig = {
       },
       {
         protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
         hostname: 'images.pexels.com',
         port: '',
         pathname: '/**',
