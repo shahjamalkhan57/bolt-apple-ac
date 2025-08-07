30077acd221b5fc3231f241d3fe3c28e9a0f4fc5
shahjamalkhan57 <106434450+shahjamalkhan57@users.noreply.github.com>
2025-08-07 21:41:48 +0500

    Updated header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -130,8 +130,8 @@ export default function Header() {
             <Image 
               src="https://i.postimg.cc/5y4MCmPd/Ginsoy-CA-1.png"
               alt="Matt Miller Contracting Logo"
-              width={50}
-              height={50}
+              width={70}
+              height={70}
               className="w-13 h-13"
             />
           </Link>
