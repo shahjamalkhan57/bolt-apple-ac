8b006db87c8a78af7f9ebf617daa27ac177856ce
shahjamalkhan57 <106434450+shahjamalkhan57@users.noreply.github.com>
2025-08-20 23:02:46 +0500

    Updated page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -156,7 +156,7 @@ export default function Home() {
             className="absolute inset-0 w-full h-full object-cover z-0"
           >
             <source
-              src="https://video-previews.elements.envatousercontent.com/files/46370c9d-cf6c-4821-bd00-4656d5929553/video_preview_h264.mp4"
+              src="https://yqpamwianchyuzleabfb.supabase.co/storage/v1/object/public/Assets/0820.mp4"
               type="video/mp4"
             />
           </video>
