f64017ac416647c5deefa37e23389c303e6202a2
Hassan Shakil <hassanshakil4569@gmail.com>
2025-09-25 20:59:26 +0500

    feat: added new favicons

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,11 @@ export const metadata: Metadata = {
   keywords:
     "roofing contractors, storm restoration, emergency roofing, Dallas Fort Worth, roof repair, roof replacement, exterior services",
   icons: {
-    icon: "/favicon.ico",
+    icon: [
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/favicon.ico" },
+    ],
   },
 };
 
