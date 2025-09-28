5f94a3d8d8cdfdd0d4dc5efaf03f5498540e35f6
Hassan Shakil <hassanshakil4569@gmail.com>
2025-09-28 22:17:10 +0500

    feat: added google scripts

--- app/layout.tsx
+++ app/layout.tsx
@@ -51,6 +51,21 @@ export default function RootLayout({
             fbq('track', 'PageView');
           `}
         </Script>
+
+        {/* Google tag (gtag.js) */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=AW-17608210417"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'AW-17608210417');
+          `}
+        </Script>
+
         <noscript>
           <img
             height="1"
